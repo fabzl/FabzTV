fca5592d4ad715bdd7191a1385bf149d12aecccd
Rento Rinalds <rento@rentodesign.com>
2015-02-26 17:21:00 +0000

    reformat message delivery

--- php/html_form_send.php
+++ php/html_form_send.php
@@ -65,114 +65,22 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 $sent = mail($email_to, $email_subject, $email_message, $headers);
-?>
 
-<!DOCTYPE html>
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Fabz.tv </title>
-    <meta name="description" content="Fabz tv portfolio - London based hotographer. Available for bookings both in the UK and abroad." />
-    <meta name="keywords" content="photographer, london, fabz, design, UK, animation, coding, designer, developer" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui">
-    <link rel="shortcut icon" href="/images/favicon.ico">
-  <script type="text/javascript" src="/js/modernizr.custom.86080.js"></script>
-
-    <!--[if lte IE 8]>
-      <link rel="stylesheet" href="css/kickoff-old-ie.css">
-    <![endif]-->
-    <!--[if gt IE 8]><!-->
-    <link rel="stylesheet" href="css/kickoff.css">
-    <!--<![endif]-->
-
-    <!-- For mobile icons etc, consider:
-       * https://gist.github.com/mrmartineau/6403816
-       * http://iconifier.net
-    -->
-
-    <script src="/js/libs/modernizr.min.js"></script>
-</head>
-
-<body>
-
-
-
-    </header>
-
-
-    <!--  ====================  -->
-    <!--  === Main content ===  -->
-    <!--  ====================  -->
-    <div class="g-container" role="main">
-      <h2>Thank you for contacting Fabz tv.</h2>
-
+// check if mail function was executed without errors
+if($sent) {
+    ?>
+    <h2>Thank you for contacting Fabz tv.</h2>
       <p>
-        We will be in touch with you very soon.
-        <br><br>
-        Kind regards
-        <br><br>
-        Fabian Andrade <br>
-        <a href="http://www.fabz.tv" target="_blank">Fabz.tv</a>
-        <br><br>
+        We will be in touch with you very soon.<br>
+        <br>
+        Kind regards<br>
+        <br>
+        Fabian Andrade
       </p>
-    </div>
-
-
-    <!--  ==============  -->
-    <!--  === Footer ===  -->
-    <!--  ==============  -->
-    <footer class="footer" role="contentinfo">
-    <div class="backtotop">
-        </div>
-        <p>2015 &copy; Fabz tv | All rights reserved</p>
-    </footer>
-
-
-    <!-- JavaScript at the bottom for fast page loading -->
-    <!--[if lte IE 8]>
-      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-      <script>window.jQuery || document.write('<script src="/js/libs/jquery.min.js"><\/script>')</script>
-    <![endif]-->
-    <!--[if gt IE 8]><!-->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-    <script>
-    window.jQuery || document.write('<script src="/js/libs/jquery.2.min.js"><\/script>')
-    </script>
-    <!--<![endif]-->
-
-    <script src="/js/dist/app.min.js"></script>
-
-    <script>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-XXXXX-XX', 'domain', {
-        //'cookieDomain': 'none' // Use if debugging on localhost
-    });
-    ga('send', 'pageview');
-    </script>
-
-</body>
-
-</html>
-
-
-
-
-
-<?php
+<?php    
+} 
+// otherwise deliver error message
+else {
+    echo "There was an error to deliver your message, please try again!";
 }
+?>
