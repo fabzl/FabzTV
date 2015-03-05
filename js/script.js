a4239cde2d402bd7919c87cd4bbe3410b9c6b425
Fabian <favalencia@tmw.co.uk>
2015-03-05 10:53:25 +0000

    loading screen fix

--- js/script.js
+++ js/script.js
@@ -135,6 +135,39 @@ Function.prototype.throttle = function (milliseconds) {
 		var url = window.location.protocol + $player.attr('src').split('?')[0];
 		var $status = $('.status');
 
+// $(function() {
+//     var iframe = $('#player1')[0];
+//     var player = $f(iframe);
+//     var status = $('.status');
+
+//     // When the player is ready, add listeners for pause, finish, and playProgress
+//     player.addEvent('ready', function() {
+//         status.text('ready');
+        
+//         player.addEvent('pause', onPause);
+//         player.addEvent('finish', onFinish);
+//         player.addEvent('playProgress', onPlayProgress);
+//     });
+
+//     // Call the API when a button is pressed
+//     $('button').bind('click', function() {
+//         player.api($(this).text().toLowerCase());
+//     });
+
+//     function onPause(id) {
+//         status.text('paused');
+//     }
+
+//     function onFinish(id) {
+//         status.text('finished');
+//     }
+
+//     function onPlayProgress(data, id) {
+//         status.text(data.seconds + 's played');
+//     }
+// });
+
+
 	$(function() {
 
 		KO.Config.init();
@@ -674,11 +707,13 @@ Function.prototype.throttle = function (milliseconds) {
 		stopPlayingvideos: function() { 
 			// post pause action
 			KO.Config.post("pause");
+			console.log("stop video");
 		},
 
 
 		// Helper function for sending a message to the vimeo player
 		post:function (action, value) {
+
 			var data = {
 				method: action
 			};
@@ -687,6 +722,7 @@ Function.prototype.throttle = function (milliseconds) {
 				data.value = value;
 			}
 
+			console.log(data);
 			var message = JSON.stringify(data);
 			$player[0].contentWindow.postMessage(data, url);
 		},
